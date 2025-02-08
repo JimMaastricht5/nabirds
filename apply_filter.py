89dcf5eeb34dc47b2004f7625141199b51e00be8
jimmaastricht <jimmaastricht5@gmail.com>
2025-02-08 09:02:59 -0600

    add documentation and code clean up

--- apply_filter.py
+++ apply_filter.py
@@ -25,6 +25,12 @@
 import csv
 
 def filter_images():
+    """
+    function loads a file from the current directory call filter.txt
+    Processes through rows and uses the class keys in this file to limit the
+    images, image_class_labels, and train_test_split files to only the desired classes in filter.txt
+    :return:
+    """
     species_class_set = set()
     image_class_set = set()
     with open('filter.txt', 'r', encoding='utf-8') as f:
@@ -33,23 +39,26 @@ def filter_images():
             class_num = int(row['class'])
             species_class_set.add(class_num)
 
-    with open('image_class_labels.txt', 'r', encoding='utf-8') as infile, open('image_class_labels_filtered.txt', 'w', encoding='utf-8') as outfile:
+    with open('image_class_labels.txt', 'r', encoding='utf-8') as infile, open('image_class_labels_filtered.txt', 'w',
+                                                                               encoding='utf-8') as outfile:
         for line in infile:
-            image_key, class_num = line.strip().split()
+            image_key, class_num = line.strip().split() # Split each line on whitespace
             class_num = int(class_num)
             if class_num in species_class_set:  # Check if class is in the set
                 outfile.write(line)
                 image_class_set.add(image_key)
 
-    with open('images.txt', 'r', encoding='utf-8') as infile, open('images_filtered.txt', 'w', encoding='utf-8') as outfile:
+    with open('images.txt', 'r', encoding='utf-8') as infile, open('images_filtered.txt', 'w',
+                                                                   encoding='utf-8') as outfile:
         for line in infile:
-            image_key, image_name = line.strip().split()  # Split each line on whitespace
+            image_key, image_name = line.strip().split()
             if image_key in image_class_set:
                 outfile.write(line)
 
-    with open('train_test_split.txt', 'r', encoding='utf-8') as infile, open('train_test_split_filtered.txt', 'w', encoding='utf-8') as outfile:
+    with open('train_test_split.txt', 'r', encoding='utf-8') as infile, open('train_test_split_filtered.txt', 'w',
+                                                                             encoding='utf-8') as outfile:
         for line in infile:
-            image_key, test_train_boolean = line.strip().split()  # Split each line on whitespace
+            image_key, test_train_boolean = line.strip().split()
             if image_key in image_class_set:
                 outfile.write(line)
 
