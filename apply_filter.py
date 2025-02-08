bef8262d3610b3cc3352d5501a6e29aa898f3229
jimmaastricht <jimmaastricht5@gmail.com>
2025-02-08 09:00:22 -0600

    add documentation

--- apply_filter.py
+++ apply_filter.py
@@ -1,3 +1,27 @@
+# MIT License
+#
+# 2024 Jim Maastricht
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# reads the NABirds text files and filters the contents based on the file filter.txt
+# jimmaastricht5@gmail.com
 import csv
 
 def filter_images():
@@ -29,5 +53,7 @@ def filter_images():
             if image_key in image_class_set:
                 outfile.write(line)
 
-filter_images()
-print('Done')
+
+if __name__ == "__main__":
+    filter_images()
+    print('Done')
